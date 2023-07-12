2066acf9ff25531f9ca5de5498d99c07591759e4
Alireza Ziaee <alrz1999@gmail.com>
2023-07-13 00:58:53 +0330

    Add sqrt

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 from enum import Enum
+import math
+
 class Calculator:
     class Operation(Enum):
         Add = 1,
         Subtract = 2,
         Multiply = 3,
         Divide = 4
+        Sqrt = 4
+
 
     @staticmethod
     def calculate(x, operation, y):
@@ -16,6 +20,8 @@ class Calculator:
             print("Result:", Calculator.multiply(x, y))
         elif operation == "Divide":
             print("Result:", Calculator.divide(x, y))
+        elif operation == "Sqrt":
+            print("Result:", Calculator.sqrt(x))
         else:
             print("Invalid choice")
             raise AttributeError
@@ -23,6 +29,7 @@ class Calculator:
     @staticmethod
     def add(x, y):
         return x + y
+
 
     @staticmethod
     def subtract(x, y):
@@ -36,22 +43,30 @@ class Calculator:
     def divide(x, y):
         pass
 
+    @staticmethod
+    def sqrt(x):
+        return math.sqrt(x)
+
 def get_input_operation():
-    operations = ["Add", "Subtract", "Multiply", "Divide"]
+    operations = ["Add", "Subtract", "Multiply", "Divide", "Sqrt"]
 
     print("Select operation:")
     for idx, operation in enumerate(operations):
         print(f"{idx+1}. {operation}")
 
-    operation_index = int(input("Enter your choice (1/2/3/4): "))
+    operation_index = int(input("Enter your choice (1/2/3/4/5): "))
     return operations[operation_index-1]
 
-def get_input_numbers():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    return num1, num2
+def get_input_numbers(operation):
+    if operation == "Sqrt":
+        num = float(input("Enter number: "))
+        return num, None
+    else:        
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        return num1, num2
 
 if __name__ == "__main__":       
     operation = get_input_operation()
-    num1, num2 = get_input_numbers()
+    num1, num2 = get_input_numbers(operation)
     Calculator.calculate(num1, operation, num2)

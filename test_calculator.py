1698c3de8737b896a3bea716533f8b6aecec6b66
Alireza Ziaee <alrz1999@gmail.com>
2023-07-12 23:56:51 +0330

    Add test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,33 @@
+import unittest
+from enum import Enum
+from calculator import Calculator
+
+class CalculatorTest(unittest.TestCase):
+    class Operation(Enum):
+        Add = 1
+        Subtract = 2
+        Multiply = 3
+        Divide = 4
+
+    def test_add(self):
+        result = Calculator.add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_subtract(self):
+        result = Calculator.subtract(5, 2)
+        self.assertEqual(result, 3)
+
+    def test_multiply(self):
+        result = Calculator.multiply(2, 3)
+        self.assertEqual(result, 6)
+
+    def test_divide(self):
+        result = Calculator.divide(6, 2)
+        self.assertEqual(result, 3)
+
+    def test_invalid_operation(self):
+        with self.assertRaises(AttributeError):
+            Calculator.calculate(2, "InvalidOperation", 3)
+
+if __name__ == '__main__':
+    unittest.main()
